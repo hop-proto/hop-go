b26bf3b31f50aa81628fd0861b55c7b015bf7a1b
Laura Bauman <baumanl@scratch-07.mgmt.esrg.stanford.edu>
2021-07-15 00:55:52 +0000

    npc attempt 1 <fail>

---
+++ npc/example/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"zmap.io/portal/certs"
+	"zmap.io/portal/channels"
+	"zmap.io/portal/keys"
+	"zmap.io/portal/npc"
+	"zmap.io/portal/transport"
+)
+
+func main() {
+	if os.Args[1] == "client" {
+		startClient(os.Args[2])
+	} else if os.Args[1] == "server" {
+		startServer(os.Args[2])
+	}
+}
+
+func principalProxy(ch *channels.Reliable) {
+	addr := "localhost:9999"
+	pktConn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		logrus.Fatalf("S: ERROR STARTING UDP CONN: %v", err)
+	}
+	// It's actually a UDP conn
+	udpConn := pktConn.(*net.UDPConn)
+	logrus.Info("principal proxy got a connection")
+	go func() {
+		io.Copy(ch, udpConn)
+	}()
+	io.Copy(udpConn, ch)
+
+}
+
+func startClient(port string) {
+	addr := "127.0.0.1:" + port
+
+	//******ESTABLISH HOP SESSION******
+	//TODO: figure out addr format requirements + check for them above
+	transportConn, err := transport.Dial("udp", addr, nil) //There seem to be limits on Dial() and addr format
+	if err != nil {
+		logrus.Fatalf("C: error dialing server: %v", err)
+	}
+	err = transportConn.Handshake()
+	if err != nil {
+		logrus.Fatalf("C: Issue with handshake: %v", err)
+	}
+	//TODO: should these functions + things from Channels layer have errors?
+	mc := channels.NewMuxer(transportConn, transportConn)
+	go mc.Start()
+	defer mc.Stop()
+
+	//start NPC channel
+	ch, e := mc.CreateChannel(channels.NPC_CHANEL)
+	if e != nil {
+		logrus.Fatalf("C: error making channel: %v", e)
+	}
+	if port == "1111" {
+		logrus.Info("Starting NPC with Server 1 on port 1111")
+		npcinit := npc.NewNPCInitMsg("127.0.0.1:2222")
+		logrus.Infof("len: %v and addr: %v", npcinit.MsgLen, npcinit.Addr)
+		ch.Write(npc.NewNPCInitMsg("127.0.0.1:2222").ToBytes())
+		ch.Read(make([]byte, 1))
+		logrus.Info("Receieved NPC Conf. Starting principal proxy...")
+		go principalProxy(ch)
+		transportConn2, err := transport.Dial("udp", "127.0.0.1:9999", nil)
+		if err != nil {
+			logrus.Fatalf("C: error dialing server: %v", err)
+		}
+		logrus.Info("Dialed Principal proxy")
+		err = transportConn2.Handshake() //hanging
+		if err != nil {
+			logrus.Fatalf("C: Issue with handshake: %v", err)
+		}
+		logrus.Info("conducted handshake")
+		//TODO: should these functions + things from Channels layer have errors?
+		mc := channels.NewMuxer(transportConn2, transportConn2)
+		go mc.Start()
+		defer mc.Stop()
+
+		mc.CreateChannel(channels.AGC_CHANNEL)
+		logrus.Info("CREATED AGC")
+	}
+	for {
+	}
+}
+
+func startServer(port string) {
+	addr := "localhost:" + port
+	pktConn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		logrus.Fatalf("S: ERROR STARTING UDP CONN: %v", err)
+	}
+	// It's actually a UDP conn
+	udpConn := pktConn.(*net.UDPConn)
+	server, err := transport.NewServer(udpConn, newTestServerConfig())
+	if err != nil {
+		logrus.Fatalf("S: ERROR STARTING TRANSPORT CONN: %v", err)
+	}
+
+	go server.Serve()
+
+	//TODO: make this a loop so it can handle multiple client conns
+	logrus.Infof("S: SERVER LISTENING ON %v", addr)
+	serverConn, err := server.AcceptTimeout(5 * time.Minute) //won't be a minute in reality
+	if err != nil {
+		logrus.Fatalf("S: SERVER TIMEOUT: %v", err)
+	}
+	logrus.Info("S: ACCEPTED NEW CONNECTION")
+	ms := channels.NewMuxer(serverConn, serverConn)
+	go ms.Start()
+	defer ms.Stop()
+	logrus.Info("S: STARTED CHANNEL MUXER")
+
+	if port == "1111" {
+		logrus.Info("Server 1: Accepting NPC and setting up Proxy to Server 2")
+		ch, _ := ms.Accept()
+
+		b := make([]byte, 4)
+		ch.Read(b)
+		l := binary.BigEndian.Uint32(b[0:4])
+		logrus.Infof("Expecting %v bytes", l)
+		init := make([]byte, l)
+		ch.Read(init)
+		dest := npc.FromBytes(init)
+		logrus.Infof("trying to dial dest: %v", dest.Addr)
+		udpConn, err := net.Dial("udp", dest.Addr)
+		if err != nil {
+			logrus.Fatalf("C: error dialing server: %v", err)
+		}
+		logrus.Info("connected to: ", dest.Addr)
+		go func() {
+			io.Copy(udpConn, ch)
+		}()
+		ch.Write([]byte{npc.NPC_CONF})
+		go func() {
+			io.Copy(ch, udpConn)
+		}()
+
+		for {
+		}
+	}
+}
+
+func newTestServerConfig() *transport.ServerConfig {
+	keyPair, err := keys.ReadDHKeyFromPEMFile("../../app/testdata/leaf-key.pem")
+	if err != nil {
+		logrus.Fatalf("S: ERROR WITH KEYPAIR %v", err)
+	}
+	certificate, err := certs.ReadCertificatePEMFile("../../app/testdata/leaf.pem")
+	if err != nil {
+		logrus.Fatalf("S: WRROR WITH CERTS %v", err)
+	}
+	intermediate, err := certs.ReadCertificatePEMFile("../../app/testdata/intermediate.pem")
+	if err != nil {
+		logrus.Fatalf("S: ERROR WITH INT CERTS %v", err)
+	}
+	return &transport.ServerConfig{
+		KeyPair:      keyPair,
+		Certificate:  certificate,
+		Intermediate: intermediate,
+	}
+}
--- npc/npc.go
+++ npc/npc.go
@@ -2,42 +2,29 @@ package npc
 
 import "encoding/binary"
 
-const NPC_INIT = byte(1)
-
-type npcMsg struct {
-	msgType byte
-	msgLen  uint32
-	d       Data
-}
-
-type Data interface {
-	toBytes() []byte
-}
+const NPC_CONF = byte(1)
 
 type npcInitMsg struct {
-	server string
-	port   string
+	MsgLen uint32
+	Addr   string
 }
 
-func NewNPCInitMsg(s string, p string) *npcMsg {
-	return &npcMsg{
-		msgType: NPC_INIT,
-		msgLen:  1 + uint32(len(s)+len(p)),
-		d: &npcInitMsg{
-			server: s,
-			port:   p,
-		},
+func NewNPCInitMsg(address string) *npcInitMsg {
+	return &npcInitMsg{
+		MsgLen: uint32(len(address)),
+		Addr:   address,
 	}
 }
 
-func (n *npcInitMsg) toBytes() []byte {
-	return append([]byte(n.server), append([]byte(" "), []byte(n.port)...)...)
+func (n *npcInitMsg) ToBytes() []byte {
+	r := make([]byte, 4)
+	binary.BigEndian.PutUint32(r, n.MsgLen)
+	return append(r, []byte(n.Addr)...)
 }
 
-func (n *npcMsg) ToBytes() []byte {
-	r := make([]byte, 5+n.msgLen)
-	r[0] = n.msgType
-	binary.BigEndian.PutUint32(r[1:5], n.msgLen)
-	copy(r[5:], n.d.toBytes())
-	return r
+func FromBytes(b []byte) *npcInitMsg {
+	return &npcInitMsg{
+		MsgLen: uint32(len(b)),
+		Addr:   string(b),
+	}
 }
