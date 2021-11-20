b077c4ee828936e65cf36446616a1f5a68b5c149
baumanl <laurabauman12@gmail.com>
2021-11-20 02:08:59 +0000

    rough impl in test

--- cmd/remotePF/main.go
+++ cmd/remotePF/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"errors"
+	"io"
 	"net"
 	"os"
 	"os/user"
 
 	"github.com/sirupsen/logrus"
+	"zmap.io/portal/netproxy"
 )
+
+const sock = "@remotesock"
 
 func main() {
 	//set up logging to file
-	_, err := os.Stat("log.txt")
-	if errors.Is(err, os.ErrNotExist) {
-		f, e := os.Create("log.txt")
-		if e != nil {
-			logrus.Error(e)
-			return
-		}
-		f.Close()
+	f, e := os.Create("log.txt")
+	if e != nil {
+		logrus.Error(e)
+		return
 	}
-	f, e := os.OpenFile("log.txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 	if e != nil {
 		logrus.Error("error opening log.txt: ", e)
 		return
@@ -34,31 +32,37 @@ func main() {
 		return
 	}
 	logrus.Infof("Child running as: %v. With UID: %v GID: %v", curUser.Username, curUser.Uid, curUser.Gid)
-	logrus.Infof("Will start TCP listener on port: %v", os.Args[1])
 
-	tcpListener, e := net.Listen("tcp", ":"+os.Args[1])
-	if e != nil {
-		logrus.Fatal(e)
+	// cudsconn, err := net.Dial("unix", sock)
+	// if err != nil {
+	// 	logrus.Error("error dialing socket", err)
+	// }
+
+	tcpListener, tcperr := net.Listen("tcp", ":"+os.Args[1])
+
+	logrus.Infof("Started TCP listener on port: %v", os.Args[1])
+	for {
+		cudsconn, err := net.Dial("unix", sock)
+		if err != nil {
+			logrus.Error("error dialing socket", err)
+		}
+		if tcperr != nil {
+			cudsconn.Write([]byte{netproxy.NpcDen})
+			logrus.Fatal(e)
+		}
+		tconn, e := tcpListener.Accept()
+		if e != nil {
+			cudsconn.Write([]byte{netproxy.NpcDen})
+			logrus.Error("error accepting tcp conn")
+			return
+		}
+		logrus.Info("Accepted TCPConn...")
+		defer tconn.Close()
+		cudsconn.Write([]byte{netproxy.NpcConf})
+		go func() {
+			io.Copy(cudsconn, tconn)
+			cudsconn.Close()
+		}()
+		io.Copy(tconn, cudsconn)
 	}
-	tcpListener.Close()
-	// if e != nil {
-	// 	logrus.Error("Issue listening on requested port")
-	// 	npTube.Write([]byte{NpcDen})
-	// 	return
-	// }
-	// tconn, e := tcpListener.Accept() //TODO(baumanl): should this be in a loop? how does SSH do it?
-	// if e != nil {
-	// 	logrus.Error("Issue accepting conn on remote port")
-	// 	npTube.Write([]byte{NpcDen})
-	// 	return
-	// }
-	// npTube.Write([]byte{NpcConf})
-	// //could net.Pipe() be useful here?
-	// go func() {
-	// 	//Handles all traffic from principal to server 2
-	// 	io.Copy(tconn, npTube)
-	// }()
-	// //handles all traffic from server 2 back to principal
-	// io.Copy(npTube, tconn)
-
 }
--- cmd/remotePF/remote_test.go
+++ cmd/remotePF/remote_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"net"
+	"os"
 	"os/exec"
 	"os/user"
 	"syscall"
@@ -9,7 +12,7 @@ import (
 	"github.com/AstromechZA/etcpwdparse"
 	"github.com/sirupsen/logrus"
 	"gotest.tools/assert"
-	"zmap.io/portal/app"
+	"zmap.io/portal/netproxy"
 )
 
 func TestRemote(t *testing.T) {
@@ -18,7 +21,7 @@ func TestRemote(t *testing.T) {
 	logrus.Infof("Currently running as: %v. With UID: %v GID: %v", curUser.Username, curUser.Uid, curUser.Gid)
 	cache, err := etcpwdparse.NewLoadedEtcPasswdCache()
 	assert.NilError(t, err)
-	args := []string{"./remotePF", app.DefaultHopPort}
+	args := []string{"./remotePF", "7778"}
 	c := exec.Command(args[0], args[1:]...)
 	if curUser.Uid == "0" {
 		logrus.Info("running as root, configuring to run as 'baumanl'")
@@ -31,13 +34,67 @@ func TestRemote(t *testing.T) {
 			Groups: []uint32{uint32(user.Gid())},
 		}
 	}
+	//set up authgrantServer (UDS socket)
+	//make sure the socket does not already exist.
+	err = os.RemoveAll(sock)
+	assert.NilError(t, err)
+
+	//set socket options and start listening to socket
+	//sockconfig := &net.ListenConfig{Control: setListenerOptions}
+	uds, err := net.Listen("unix", sock)
+	assert.NilError(t, err)
+	defer uds.Close()
+	logrus.Infof("address: %v", uds.Addr())
+
 	// r, err := c.StdoutPipe()
 	// assert.NilError(t, err)
+
 	// w, err := c.StdinPipe()
 	// assert.NilError(t, err)
 	// _, err = c.StderrPipe()
 	// assert.NilError(t, err)
 
-	err = c.Run()
+	err = c.Start()
+	assert.NilError(t, err)
+
+	udsconn, err := uds.Accept()
+	assert.NilError(t, err)
+
+	logrus.Info("got first conn")
+	buf := make([]byte, 1)
+	_, err = udsconn.Read(buf)
+	assert.NilError(t, err)
+	if buf[0] == netproxy.NpcConf {
+		logrus.Info("got conf")
+		go func() {
+			io.Copy(os.Stdout, udsconn)
+			udsconn.Close()
+			logrus.Info("closed first conn")
+		}()
+	}
+
+	udsconn, err = uds.Accept()
+	assert.NilError(t, err)
+	logrus.Info("got second conn")
+	buf = make([]byte, 1)
+	_, err = udsconn.Read(buf)
+	assert.NilError(t, err)
+	if buf[0] == netproxy.NpcConf {
+		logrus.Info("got conf")
+		go func() {
+			io.Copy(os.Stdout, udsconn)
+			udsconn.Close()
+		}()
+	}
+	// assert.NilError(t, err)
+	// buf := make([]byte, 10)
+	// n, _ := udsconn.Read(buf)
+	// assert.Equal(t, n, 10)
+	// assert.Equal(t, string(buf), "Hi there!!")
+	// logrus.Infof("received: %v bytes : %v", n, string(buf))
+	// err = udsconn.Close()
+	// assert.NilError(t, err)
+
+	err = c.Wait()
 	assert.NilError(t, err)
 }
