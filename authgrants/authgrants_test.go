991ffdb599154fa03dac884febb529abf7a911d7
baumanl <laurabauman12@gmail.com>
2021-12-16 01:32:42 +0000

    change authgrants_test to run over tcp instead of hop for ease of config

--- authgrants/authgrants_test.go
+++ authgrants/authgrants_test.go
@@ -4,18 +4,11 @@ import (
 	"net"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/sirupsen/logrus"
 	"gotest.tools/assert"
-	"zmap.io/portal/certs"
-	"zmap.io/portal/keys"
 	"zmap.io/portal/ports"
-	"zmap.io/portal/transport"
-	"zmap.io/portal/tubes"
 )
-
-const testDataPathPrefix = "../transport/"
 
 var start = 17000
 
@@ -29,93 +22,28 @@ func port() string {
 	return port
 }
 
-//NewTestServerConfig populates server config and verify config with sample cert data
-func newTestServerConfig(testDataPathPrefix string) (transport.ServerConfig, transport.VerifyConfig) {
-	keyPair, err := keys.ReadDHKeyFromPEMFile(testDataPathPrefix + "testdata/leaf-key.pem")
-	if err != nil {
-		logrus.Fatalf("S: ERROR WITH KEYPAIR %v", err)
-	}
-	certificate, err := certs.ReadCertificatePEMFile(testDataPathPrefix + "testdata/leaf.pem")
-	if err != nil {
-		logrus.Fatalf("S: ERROR WITH CERTS %v", err)
-	}
-	intermediate, err := certs.ReadCertificatePEMFile(testDataPathPrefix + "testdata/intermediate.pem")
-	if err != nil {
-		logrus.Fatalf("S: ERROR WITH INT CERTS %v", err)
-	}
-	root, err := certs.ReadCertificatePEMFile(testDataPathPrefix + "testdata/root.pem")
-	if err != nil {
-		logrus.Fatalf("S: ERROR WITH ROOT CERT %v", err)
-	}
-	err = certs.VerifyParent(certificate, intermediate)
-	if err != nil {
-		logrus.Fatal("Verify Parent Issue: ", err)
-	}
-	err = certs.VerifyParent(intermediate, root)
-	if err != nil {
-		logrus.Fatal("Verify Parent Issue: ", err)
-	}
-	err = certs.VerifyParent(root, root)
-	if err != nil {
-		logrus.Fatal("Verify Parent Issue: ", err)
-	}
-
-	server := transport.ServerConfig{
-		KeyPair:      keyPair,
-		Certificate:  certificate,
-		Intermediate: intermediate,
-	}
-	verify := transport.VerifyConfig{
-		Store: certs.Store{},
-	}
-	verify.Store.AddCertificate(root)
-	return server, verify
-}
-
-func getInsecureClientConfig() transport.ClientConfig {
-	return transport.ClientConfig{
-		Verify: transport.VerifyConfig{
-			InsecureSkipVerify: true,
-		},
-	}
-}
-
 func TestIntentRequest(t *testing.T) {
+	wg := sync.WaitGroup{}
 	logrus.SetLevel(logrus.DebugLevel)
 	port := port()
-	pktConn, err := net.ListenPacket("udp", net.JoinHostPort("localhost", port))
-	assert.NilError(t, err)
-	// It's actually a UDP conn
-	udpConn := pktConn.(*net.UDPConn)
-	s, _ := newTestServerConfig(testDataPathPrefix)
-	server, err := transport.NewServer(udpConn, s)
-	assert.NilError(t, err)
-	go server.Serve()
-
-	transportConn, err := transport.Dial("udp", udpConn.LocalAddr().String(), getInsecureClientConfig())
+	tcpListener, err := net.Listen("tcp", net.JoinHostPort("localhost", port))
 	assert.NilError(t, err)
 
-	assert.NilError(t, transportConn.Handshake())
-
-	serverConn, err := server.AcceptTimeout(time.Minute)
+	clientConn, err := net.Dial("tcp", ":"+port)
 	assert.NilError(t, err)
 
-	mc := tubes.NewMuxer(transportConn, transportConn)
-	go mc.Start()
-
-	agc, err := NewAuthGrantConnFromMux(mc)
+	serverConn, err := tcpListener.Accept()
 	assert.NilError(t, err)
+	defer serverConn.Close()
 
-	ms := tubes.NewMuxer(serverConn, serverConn)
-	go ms.Start()
+	agc := NewAuthGrantConn(clientConn)
 
-	stube, err := ms.Accept()
-	assert.NilError(t, err)
-	sagc := &AuthGrantConn{conn: stube}
-
+	sagc := &AuthGrantConn{conn: serverConn}
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		ir := newIntent([32]byte{}, "user", "host", "port", 2, "myCmd")
-		logrus.Info("C: Made req: \n",
+		logrus.Info("C: Made req: ",
 			"clientsni: ", ir.clientSNI, " ",
 			"client user: ", ir.clientUsername, " ",
 			"port: ", ir.port, " ",
@@ -130,16 +58,17 @@ func TestIntentRequest(t *testing.T) {
 		assert.NilError(t, err)
 		switch rtype {
 		case IntentConfirmation:
-			logrus.Info("C: Got conf with deadline: ", fromIntentConfirmationBytes(response).deadline)
+			logrus.Info("C: Got conf with deadline: ", fromIntentConfirmationBytes(response[dataOffset:]).deadline)
 		case IntentDenied:
-			logrus.Info("C: Got den with reason: ", fromIntentDeniedBytes(response).reason)
+			logrus.Infof("C: Got den with reason: %v", fromIntentDeniedBytes(response[dataOffset:]).reason)
+			assert.Equal(t, fromIntentDeniedBytes(response[dataOffset:]).reason, "because I say so")
 		}
 		agc.Close()
 	}()
 
 	ir, err := sagc.GetIntentRequest()
 	assert.NilError(t, err)
-	logrus.Info("S: Got req: \n",
+	logrus.Info("S: Got req: ",
 		"clientsni: ", ir.clientSNI, " ",
 		"client user: ", ir.clientUsername, " ",
 		"port: ", ir.port, " ",
@@ -147,9 +76,8 @@ func TestIntentRequest(t *testing.T) {
 		"serverUser: ", ir.serverUsername, " ",
 		"grantType: ", ir.actionType, " ",
 		"sha3: ", ir.sha3)
-	//err = SendIntentConf(stube, time.Now())
+	//err = sagc.SendIntentConf(time.Now())
 	err = sagc.SendIntentDenied("because I say so")
 	assert.NilError(t, err)
-	stube.Close()
-
+	wg.Wait()
 }
