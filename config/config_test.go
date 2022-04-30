aeb00b925dad5ae76a76c6674f22b8cf7850a22d
baumanl <laurabauman12@gmail.com>
2022-04-30 21:33:25 +0000

    added test of GetClientCopy

--- config/config_test.go
+++ config/config_test.go
@@ -19,3 +19,11 @@ func TestLoadServerConfig(t *testing.T) {
 	t.Log(*c)
 	// TODO(dadrian): Actually check the output
 }
+
+func TestConfigCopy(t *testing.T) {
+	err := InitClient("ast/testdata/client")
+	assert.NilError(t, err)
+	cc := GetClient()
+	copy := GetClientCopy("example.localhost")
+	assert.Equal(t, cc.MatchHost("example.localhost").Hostname, copy.Hosts[0].Hostname)
+}
