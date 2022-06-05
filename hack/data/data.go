285394daaec91013412a96a0a7ddb93e37e65531
David Adrian <davidcadrian@gmail.com>
2022-06-05 16:02:57 -0600

    remove unused code in hack/ dir

--- hack/data/data.go
+++ hack/data/data.go
@@ -4,11 +4,7 @@
 package data
 
 import (
-	"crypto/sha256"
-	"encoding/json"
 	"errors"
-	"io"
-	"io/fs"
 	"os"
 	"path/filepath"
 	"sync"
@@ -38,70 +34,4 @@ func Workspace() string {
 		}
 	})
 	return workspaceDir
-}
-
-// Data is not-quite a content addressable store
-type Data struct {
-	m       sync.Mutex
-	root    string
-	fsystem fs.FS
-}
-
-var instance *Data
-var initOnce sync.Once
-
-// Instance returns the default global instance for the workspace
-func Instance() *Data {
-	initOnce.Do(func() {
-		root := filepath.Join(Workspace(), ".local")
-		instance = &Data{
-			root:    root,
-			fsystem: os.DirFS(root),
-		}
-	})
-	return instance
-}
-
-// Hash is a byte array representing a SHA-256
-type Hash [sha256.Size]byte
-
-// State is a map of names to hashes
-type State map[string]Hash
-
-// State returns the current data state.
-func (d *Data) State() (State, error) {
-	var s State
-	d.m.Lock()
-	defer d.m.Unlock()
-	f, err := os.Open(filepath.Join(d.root, "db.json"))
-	if err != nil {
-		return nil, err
-	}
-	err = json.NewDecoder(f).Decode(&s)
-	return s, err
-}
-
-// Get retrieves the named object as bytes
-func (d *Data) Get(name string) ([]byte, error) {
-	f, err := d.fsystem.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	return io.ReadAll(f)
-}
-
-// NewResource writes out a new resource for during calls to Gen.
-type NewResource struct {
-	Name      string
-	Generator func(w io.Writer) error
-}
-
-// Gen overwrites a resource
-func (d *Data) Gen(res NewResource) error {
-	p := filepath.Join(d.root, res.Name)
-	f, err := os.Create(p)
-	if err != nil {
-		return err
-	}
-	return res.Generator(f)
 }
