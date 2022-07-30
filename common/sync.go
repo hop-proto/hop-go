82a3490d8b8715847f2f71116cc7e985d8f57226
George Hosono <georgehosono@gmail.com>
2022-07-29 17:29:22 -0700

    passes nettests one way but not the other

--- common/sync.go
+++ common/sync.go
@@ -318,6 +318,15 @@ type DeadlineChan struct {
 }
 
 func (d *DeadlineChan) Recv() (b []byte , err error) {
+
+	// Return buffered data even if the channel is canceled
+	select {
+	case b = <-d.C:
+		return
+	default:
+		break
+	}
+
 	errChan := d.deadline.Done()
 	select{
 	case err = <-errChan:
@@ -357,7 +366,6 @@ func (d *DeadlineChan) Cancel(err error) {
 
 func (d *DeadlineChan) Close() {
 	d.deadline.Cancel(io.EOF)
-	close(d.C)
 }
 
 func NewDeadlineChan(size int) *DeadlineChan {
