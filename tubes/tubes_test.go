bf1ba780421e60ef74d0872306929fa0baead715
George Hosono <georgehosono@gmail.com>
2022-08-08 14:35:11 -0700

    Started test tubes

--- tubes/tubes_test.go
+++ tubes/tubes_test.go
@@ -4,35 +4,99 @@ package tubes
 import (
 	"net"
 	"testing"
+	"time"
 
 	"golang.org/x/net/nettest"
 	"gotest.tools/assert"
 
-	"hop.computer/hop/hoptests"
+	"hop.computer/hop/certs"
+	"hop.computer/hop/common"
+	"hop.computer/hop/keys"
+	"hop.computer/hop/transport"
 )
 
-func makeConn(t *testing.T) (c1, c2 net.Conn, stop func(), err error){
-		s := hoptests.NewTestServer(t)
-		c := hoptests.NewTestClient(t, s, "username")
+func newServerConfig(t *testing.T) (transport.ServerConfig, transport.VerifyConfig) {
+	config := transport.ServerConfig{
+		HandshakeTimeout: 1 * time.Second,
+	}
 
-		s.AddClientToAuthorizedKeys(t, c)
+	config.KeyPair = keys.GenerateNewX25519KeyPair()
+	intermediateKeyPair := keys.GenerateNewSigningKeyPair()
+	rootKeyPair := keys.GenerateNewSigningKeyPair()
 
-		s.StartTransport(t)
-		s.StartHopServer(t)
+	root, err := certs.SelfSignRoot(certs.SigningIdentity(rootKeyPair), rootKeyPair)
+	root.ProvideKey((*[32]byte)(&rootKeyPair.Private))
+	assert.NilError(t, err)
 
-		c.StartClient(t)
+	config.Intermediate, err = certs.IssueIntermediate(root, certs.SigningIdentity(intermediateKeyPair))
+	config.Intermediate.ProvideKey((*[32]byte)(&intermediateKeyPair.Private))
+	assert.NilError(t, err)
 
-		c1, err = c.TubeMuxer.CreateTube(Reliable)
-		assert.NilError(t, err)
-		// TODO(hosono) accept tube on other end
+	config.Certificate, err = certs.IssueLeaf(config.Intermediate, certs.LeafIdentity(config.KeyPair, certs.DNSName("example.local")))
+	assert.NilError(t, err)
 
-		return
+	verify := transport.VerifyConfig{
+		Store: certs.Store{},
+	}
+	verify.Store.AddCertificate(root)
+
+	return config, verify
+}
+
+func makeTubeConn(t *testing.T) (c1, c2 net.Conn, stop func(), err error){
+	serverUDP, err := net.ListenUDP("udp", nil)
+	assert.NilError(t, err)
+
+	serverConfig, verify := newServerConfig(t)
+	server, err := transport.NewServer(serverUDP, serverConfig)
+	assert.NilError(t, err)
+
+	go server.Serve()
+
+	keypair := keys.GenerateNewX25519KeyPair()
+	assert.NilError(t, err)
+	leaf, err := certs.SelfSignLeaf(&certs.Identity{
+		PublicKey: keypair.Public,
+	})
+	assert.NilError(t, err)
+	clientConfig := transport.ClientConfig{
+		Verify: verify,	
+		Exchanger: keypair,
+		Leaf: leaf,
+	}
+	client, err := transport.Dial("udp", serverUDP.LocalAddr().String(), clientConfig)
+	assert.NilError(t, err)
+
+	err = client.Handshake()
+	assert.NilError(t, err)
+
+	handle, err := server.AcceptTimeout(time.Second)
+	assert.NilError(t, err)
+
+	clientMuxer := NewMuxer(client, client, time.Second)
+	serverMuxer := NewMuxer(handle, handle, time.Second)
+	go clientMuxer.Start()
+	go serverMuxer.Start()
+
+	c1, err = clientMuxer.CreateTube(common.ExecTube)
+	assert.NilError(t, err)
+
+	c2, err = serverMuxer.Accept()
+	assert.NilError(t, err)
+
+	stop = func() {
+		c1.Close()
+		c2.Close()
+		client.Close()
+		server.Close()
+	}
+	return
 }
 
 func TestTubes(t *testing.T) {
 	mk := nettest.MakePipe(
 		func() (c1, c2 net.Conn, stop func(), err error) {
-			return makeConn(t)
+			return makeTubeConn(t)
 	})
 	nettest.TestConn(t, mk)
 }
