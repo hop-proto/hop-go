68145d6c8f47cd6a6d6d1949dcf798f976ff561a
George Hosono <georgehosono@gmail.com>
2022-08-15 17:15:19 -0700

    Reliable tubes pass BasicIO

--- tubes/sender.go
+++ tubes/sender.go
@@ -2,15 +2,17 @@ package tubes
 
 import (
 	"errors"
+	"io"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
 // The largest tube frame data length field.
-const maxFrameDataLength = 2000
+const maxFrameDataLength uint16 = 2000
 
 // The highest number of frames we will transmit per timeout period,
 // even if the window size is large enough.
@@ -24,17 +26,13 @@ type sender struct {
 	frameNo uint32
 	// +checklocks:l
 	windowSize uint16
+	
 	// +checklocks:l
 	finSent bool
-	// +checklocks:l
-	closed bool
+	closed atomic.Bool
 	// The buffer of unacknowledged tube frames that will be retransmitted if necessary.
 	// +checklocks:l
 	frames []*frame
-
-	//TODO(baumanl): is it safe and good style to have this tube here?
-	//First attempt at stopping laggy behavior due to retransmit previously always waiting for RTO.
-	ret chan int //signaled whenever frame added to frames so retransmit() starts
 
 	// Different frames can have different data lengths -- we need to know how
 	// to update the buffer when frames are acknowledged.
@@ -51,6 +49,7 @@ type sender struct {
 	// The lock controls all fields of the sender.
 	l sync.Mutex
 	// Retransmission TimeOut.
+	RTOTicker *time.Ticker
 	RTO       time.Duration
 	sendQueue chan *frame
 }
@@ -64,7 +63,7 @@ func (s *sender) unsentFramesRemaining() bool {
 func (s *sender) write(b []byte) (n int, err error) {
 	s.l.Lock()
 	defer s.l.Unlock()
-	if s.closed {
+	if s.closed.Load() {
 		return 0, errors.New("trying to write to closed tube")
 	}
 	s.buffer = append(s.buffer, b...)
@@ -84,15 +83,16 @@ func (s *sender) write(b []byte) (n int, err error) {
 		s.frameNo++
 		s.buffer = s.buffer[dataLength:]
 		s.frames = append(s.frames, &pkt)
-
 	}
 
-	s.ret <- 1
+	for i := 0; i < len(s.frames) && i < int(s.windowSize); i++{
+		s.sendQueue <- s.frames[i]
+	}
+	s.RTOTicker.Reset(s.RTO)
 	return len(b), nil
 }
 
 func (s *sender) recvAck(ackNo uint32) error {
-	logrus.Debug("GRABBING LOCK")
 	s.l.Lock()
 	defer s.l.Unlock()
 	newAckNo := uint64(ackNo)
@@ -116,10 +116,9 @@ func (s *sender) recvAck(ackNo uint32) error {
 }
 
 func (s *sender) retransmit() {
-	ticker := time.NewTicker(s.RTO)
-	for !s.isClosed() { // TODO - decide how to shutdown this endless loop with an enum state
+	for !s.closed.Load() { // TODO - decide how to shutdown this endless loop with an enum state
 		select {
-		case <-ticker.C:
+		case <-s.RTOTicker.C:
 			s.l.Lock()
 			if len(s.frames) == 0 { // Keep Alive messages
 				pkt := frame{
@@ -127,46 +126,30 @@ func (s *sender) retransmit() {
 					frameNo:    s.frameNo,
 					data:       []byte{},
 				}
-				//logrus.Info("SENDING EMPTY PACKET ON SEND QUEUE FOR ACK - FIN? ", pkt.flags.FIN)
+				// logrus.Info("SENDING EMPTY PACKET ON SEND QUEUE FOR ACK - FIN? ", pkt.flags.FIN)
 				s.sendQueue <- &pkt
 			}
 			for i := 0; i < len(s.frames) && i < int(s.windowSize) && i < maxFragTransPerRTO; i++ {
 				s.sendQueue <- s.frames[i]
-				//logrus.Info("PUTTING PKT ON SEND QUEUE - FIN? ", s.frames[i].flags.FIN)
-			}
-			s.l.Unlock()
-		case <-s.ret: //case received new data
-			s.l.Lock()
-			ticker.Reset(s.RTO)
-			i := 0
-			for i < len(s.frames) && i < int(s.windowSize) && i < maxFragTransPerRTO {
-				s.sendQueue <- s.frames[i]
-				//logrus.Info("PUTTING PKT ON SEND QUEUE - FIN? ", s.frames[i].flags.FIN)
-				i++
+				 //logrus.Info("PUTTING PKT ON SEND QUEUE - FIN? ", s.frames[i].flags.FIN)
 			}
 			s.l.Unlock()
 		}
-
 	}
 }
 func (s *sender) isClosed() bool {
-	s.l.Lock()
-	defer s.l.Unlock()
-	return s.closed
+	return s.closed.Load()
 }
 
-func (s *sender) close() {
-	s.l.Lock()
-	defer s.l.Unlock()
-	s.closed = true
+func (s *sender) Close() {
+	s.closed.Store(true)
 }
 
-/* */
 func (s *sender) sendFin() error {
 	s.l.Lock()
 	defer s.l.Unlock()
-	if s.closed || s.finSent {
-		return errors.New("tube is already closed")
+	if s.closed.Load() || s.finSent {
+		return fmt.Errorf("tube already closed [%w]", io.EOF)
 	}
 	s.finSent = true
 
@@ -185,7 +168,7 @@ func (s *sender) sendFin() error {
 	s.frameDataLengths[pkt.frameNo] = 0
 	s.frameNo++
 	s.frames = append(s.frames, &pkt)
-	s.ret <- 1
+	s.sendQueue <- &pkt
 	//logrus.Info("ADDED FIN PACKET TO SEND QUEUE")
 	return nil
 }
