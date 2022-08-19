7d55e67ee71b839883f7a26a9930cb120739799a
Daniel Rebelsky <4641927+drebelsky@users.noreply.github.com>
2022-08-19 11:11:21 -0700

    Update config tests

--- config/config_test.go
+++ config/config_test.go
@@ -7,15 +7,31 @@ import (
 )
 
 func TestLoadClientConfig(t *testing.T) {
-	c, err := LoadClientConfigFromFile("ast/testdata/client")
+	c, err := LoadClientConfigFromFile("testdata/client")
 	assert.NilError(t, err)
-	t.Log(*c)
-	// TODO(dadrian): Actually check the output
+	expected := &ClientConfig{
+		Global: HostConfig{
+			CAFiles: []string{"/path/to/ca.pem", "/path/to/other.pem"},
+		},
+		Hosts: []HostConfig{HostConfig{
+			Patterns:     []string{"example.localhost"},
+			Key:          "/path/to/key.pem",
+			Certificate:  "/path/to/cert.pem",
+			AutoSelfSign: False,
+			Hostname:     "example.localhost",
+			Port:         1234,
+		}},
+	}
+	assert.DeepEqual(t, c, expected)
 }
 
 func TestLoadServerConfig(t *testing.T) {
-	c, err := LoadServerConfigFromFile("ast/testdata/server")
+	c, err := LoadServerConfigFromFile("testdata/server")
 	assert.NilError(t, err)
-	t.Log(*c)
-	// TODO(dadrian): Actually check the output
+  expected := &ServerConfig {
+    ListenAddress: ":77",
+    Key: "/etc/hopd/id_hop.pem",
+    Certificate: "/etc/hopd/id_hop.cert",
+  }
+  assert.DeepEqual(t, c, expected)
 }
