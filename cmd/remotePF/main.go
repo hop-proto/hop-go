1bd29fa15e5e1a2d7d16be37f00e727eaa3e501f
Daniel Rebelsky <4641927+drebelsky@users.noreply.github.com>
2022-12-14 15:05:17 -0800

    Pass file descriptors over unix socket

---
+++ cmd/remotePF/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+	"os"
+	"syscall"
+)
+
+// modified lightly from https://github.com/ftrvxmtrx/fd
+// Put sends file descriptors to Unix domain socket.
+//
+// Please note that the number of descriptors in one message is limited
+// and is rather small.
+// Use conn.File() to get a file if you want to put a network connection.
+func Put(via *net.UnixConn, files ...*os.File) error {
+	if len(files) == 0 {
+		return nil
+	}
+
+	viaf, err := via.File()
+	if err != nil {
+		return err
+	}
+	socket := int(viaf.Fd())
+	// defer viaf.Close()
+
+	fds := make([]int, len(files))
+	for i := range files {
+		fds[i] = int(files[i].Fd())
+	}
+
+	rights := syscall.UnixRights(fds...)
+	return syscall.Sendmsg(socket, nil, rights, nil, 0)
+}
+
+func PutFD(via *net.UnixConn, fds []int) error {
+	if len(fds) == 0 {
+		return nil
+	}
+
+	viaf, err := via.File()
+	if err != nil {
+		return err
+	}
+	socket := int(viaf.Fd())
+	defer viaf.Close()
+
+	rights := syscall.UnixRights(fds...)
+	return syscall.Sendmsg(socket, nil, rights, nil, 0)
+}
+
+func main() {
+  // TODO(drebelsky): better error handling
+	conno, err := net.Dial("unix", "/tmp/sock")
+	if err != nil {
+		panic(err)
+	}
+	conn := conno.(*net.UnixConn)
+	for {
+		var length uint16
+		err = binary.Read(conn, binary.BigEndian, &length)
+		if err != nil {
+			panic(err)
+		}
+		b := make([]byte, length)
+		_, err = io.ReadFull(conn, b)
+		if err != nil {
+			panic(err)
+		}
+		netType := string(b)
+		err = binary.Read(conn, binary.BigEndian, &length)
+		if err != nil {
+			panic(err)
+		}
+		b2 := make([]byte, length)
+		_, err = io.ReadFull(conn, b2)
+		if err != nil {
+			panic(err)
+		}
+		addr := string(b2)
+		ln, err := net.Listen(netType, addr)
+		if err != nil {
+			panic(err)
+		}
+		switch server := ln.(type) {
+		case *net.TCPListener:
+			file, err := server.File()
+			if err != nil {
+				panic(err)
+			}
+			Put(conn, file)
+			file.Close()
+		case *net.UnixListener:
+			file, err := server.File()
+			if err != nil {
+				panic(err)
+			}
+			Put(conn, file)
+			file.Close()
+		default:
+			panic("Bad type")
+		}
+		ln.Close()
+	}
+}
---
+++ portforwarding/recv_conn.go
@@ -0,0 +1,61 @@
+package portforwarding
+// modified lightly from https://github.com/ftrvxmtrx/fd
+
+import (
+	"net"
+	"os"
+	"syscall"
+)
+
+// Get receives file descriptors from a Unix domain socket.
+//
+// Num specifies the expected number of file descriptors in one message.
+// Internal files' names to be assigned are specified via optional filenames
+// argument.
+//
+// You need to close all files in the returned slice. The slice can be
+// non-empty even if this function returns an error.
+//
+// Use net.FileConn() if you're receiving a network connection.
+func Get(via *net.UnixConn, num int, filenames []string) ([]*os.File, error) {
+	if num < 1 {
+		return nil, nil
+	}
+
+	// get the underlying socket
+	viaf, err := via.File()
+	if err != nil {
+		return nil, err
+	}
+	socket := int(viaf.Fd())
+	defer viaf.Close()
+
+	// recvmsg
+	buf := make([]byte, syscall.CmsgSpace(num*4))
+	_, _, _, _, err = syscall.Recvmsg(socket, nil, buf, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse control msgs
+	var msgs []syscall.SocketControlMessage
+	msgs, err = syscall.ParseSocketControlMessage(buf)
+
+	// convert fds to files
+	res := make([]*os.File, 0, len(msgs))
+	for i := 0; i < len(msgs) && err == nil; i++ {
+		var fds []int
+		fds, err = syscall.ParseUnixRights(&msgs[i])
+
+		for fi, fd := range fds {
+			var filename string
+			if fi < len(filenames) {
+				filename = filenames[fi]
+			}
+
+			res = append(res, os.NewFile(uintptr(fd), filename))
+		}
+	}
+
+	return res, err
+}
