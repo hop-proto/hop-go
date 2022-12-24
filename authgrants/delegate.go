c13d3365b5a6c83036a4ef1fd01ca89fcd4832f9
baumanl <laurabauman12@gmail.com>
2022-12-24 09:11:24 +0000

    fixed comment style

--- authgrants/delegate.go
+++ authgrants/delegate.go
@@ -7,15 +7,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-/* Delegate client: a hop client that descends from a Delegate proxy server
- * session with a Principal client. Requests an authorization grant from the
- * principal to connect to some Target server and perform some action(s).
- *
- * Responsibilities [status]:
- * - connect to Delegate proxy server unix socket [TODO]
- * - create and send Intent Requests [TODO]
- *
- */
+// Delegate client: a hop client that descends from a Delegate proxy server
+//  session with a Principal client. Requests an authorization grant from the
+//  principal to connect to some Target server and perform some action(s).
+//
+//  Responsibilities [status]:
+//  - connect to Delegate proxy server unix socket
+//  - create and send Intent Requests
 
 type delegateInstance struct {
 	principalConn  net.Conn
