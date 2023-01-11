37b83e1137f3e9758dd0b84434836c2cc97067d8
baumanl <laurabauman12@gmail.com>
2023-01-11 17:46:43 +0000

    added transport layer debug messages

--- transport/server.go
+++ transport/server.go
@@ -439,13 +439,16 @@ func (s *Server) handleClientAuth(b []byte, addr *net.UDPAddr) (int, *HandshakeS
 
 	// skips all verification if certVerify nil or explicitly disabled
 	if hs.certVerify != nil && !hs.certVerify.InsecureSkipVerify {
+		logrus.Debug("server: beginning client authentication")
 		// try authkeys first if enabled
 		var errAuthkeys error
 		if hs.certVerify.AuthKeysAllowed {
+			logrus.Debug("server: authkeys are allowed. attempting to validate self-signed cert")
 			errAuthkeys = hs.certVerify.AuthKeys.VerifyLeaf(&leaf, opts)
 		}
 		if !hs.certVerify.AuthKeysAllowed || errAuthkeys != nil {
 			// Certificate Verification
+			logrus.Debug("server: attempting cert validation")
 			err := hs.certVerify.Store.VerifyLeaf(&leaf, opts)
 			if err != nil { // cert verifification failed
 				logrus.Errorf("server: client authentication failed")
@@ -455,6 +458,8 @@ func (s *Server) handleClientAuth(b []byte, addr *net.UDPAddr) (int, *HandshakeS
 		} else {
 			logrus.Info("server: client used an authorized key to authenticate")
 		}
+	} else {
+		logrus.Debug("server: certVerify nil or InsecureSkipVerify set. skipping all client authentication")
 	}
 
 	hs.se, err = hs.ephemeral.DH(leaf.PublicKey[:])
