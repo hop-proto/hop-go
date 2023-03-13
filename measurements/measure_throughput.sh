5022480ebbad9ded4b15d94992ea23d85c59fec5
George Hosono <georgehosono@gmail.com>
2023-03-13 04:56:54 -0700

    throughput measurements work properly now

--- measurements/measure_throughput.sh
+++ measurements/measure_throughput.sh
@@ -3,35 +3,38 @@
 chmod 600 ./containers/ssh_id_ed25519
 export TIMEFORMAT="%3R"
 
-output_file=throughput.csv
-rm $output_file
+hop_output=hop_throughput.csv
+ssh_output=ssh_throughput.csv
+
+echo -e "file size (bytes),time to transfer (seconds)" > $hop_output
+echo -e "file size (bytes),time to transfer (seconds)" > $ssh_output
+
+filename_to_bytes() {
+    du -b $1 | cut -f 1
+}
 
 # takes in the name of a file to transfer
 hop_transfer_file() {
-    echo -n "hop time $1 (seconds)," >> $output_file
+    size=$(filename_to_bytes $1)
     for i in {1..100}; do
         echo "hop $1 $i"
+        echo -n "$size," >> $hop_output
         { time ./hop -C ./containers/client_config \
-            hop://root@127.0.0.1:7777 "cat > $1" < $1; } 2>&1 \
-            | tr -d '\n' >> $output_file
-
-        echo -n "," >> $output_file
+            hop://root@127.0.0.1:7777 "cat > $1" < $1; } \
+            2>>$hop_output
     done
-    echo "" >> $output_file
 }
 
 # takes in the name of a file to transfer
 ssh_transfer_file() {
-    echo -n "ssh time $1 (seconds)," >> $output_file
+    size=$(filename_to_bytes $1)
     for i in {1..100}; do
         echo "ssh $1 $i"
+        echo -n "$size," >> $ssh_output
         { time scp -q -i ./containers/ssh_id_ed25519 -P 7777 \
-            $1 root@127.0.0.1:$1; } 2>&1 \
-            | tr -d '\n' >> $output_file
-
-        echo -n "," >> $output_file
+            $1 root@127.0.0.1:$1; } \
+            2>>$ssh_output
     done
-    echo "" >> $output_file
 }
 
 hop_transfer_file 1kB_file
