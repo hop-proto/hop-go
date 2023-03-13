6fd8173e1e6795bc70e40d44a1285838ce5ca216
George Hosono <georgehosono@gmail.com>
2023-03-13 01:51:43 -0700

    include 10GB file for throughput measurements

--- measurements/measure_throughput.sh
+++ measurements/measure_throughput.sh
@@ -9,6 +9,7 @@ rm $output_file
 hop_transfer_file() {
     echo -n "hop time $1 (seconds)," >> $output_file
     for i in {1..100}; do
+        echo "hop $1 $i"
         { time ./hop -C ./containers/client_config \
             hop://root@127.0.0.1:7777 "cat > $1" < $1; } 2>&1 \
             | tr -d '\n' >> $output_file
@@ -21,8 +22,9 @@ hop_transfer_file() {
 ssh_transfer_file() {
     echo -n "ssh time $1 (seconds)," >> $output_file
     for i in {1..100}; do
-        { time ssh -i ./containers/ssh_id_ed25519 -p 7777 \
-            root@127.0.0.1 "cat > $1" < $1; } 2>&1 \
+        echo "ssh $1 $i"
+        { time scp -q -i ./containers/ssh_id_ed25519 -P 7777 \
+            $1 root@127.0.0.1:$1; } 2>&1 \
             | tr -d '\n' >> $output_file
 
         echo -n "," >> $output_file
@@ -41,3 +43,6 @@ ssh_transfer_file 100MB_file
 
 hop_transfer_file 1GB_file
 ssh_transfer_file 1GB_file
+
+hop_transfer_file 10GB_file
+ssh_transfer_file 10GB_file
