fb449544df071464b7c831059428a81431bc3911
George Hosono <georgehosono@gmail.com>
2023-04-01 17:35:20 -0700

    updates to measurement scripts

--- measurements/measure_throughput.sh
+++ measurements/measure_throughput.sh
@@ -6,8 +6,8 @@ export TIMEFORMAT="%3R"
 hop_output=hop_throughput.csv
 ssh_output=ssh_throughput.csv
 
-echo -e "file size (bytes),time to transfer (seconds)" > $hop_output
-echo -e "file size (bytes),time to transfer (seconds)" > $ssh_output
+echo -e "file size (bytes),time to transfer (microseconds)" > $hop_output
+echo -e "file size (bytes),time to transfer (microseconds)" > $ssh_output
 
 filename_to_bytes() {
     du -b $1 | cut -f 1
@@ -19,9 +19,10 @@ hop_transfer_file() {
     for i in {1..100}; do
         echo "hop $1 $i"
         echo -n "$size," >> $hop_output
-        { time ./hop -C ./containers/client_config \
-            $2 "cat > $1" < $1; } \
-            2>>$hop_output
+        ./pty_time/pty_time matches \
+            bash -c "./hop -C ./containers/client_config $2 \"cat $1 && echo 'done echoing file'\" | grep -m 1 \"done echoing file\"" \
+            | sed -nr 's/diff: ([[:digit:]]+) microseconds/\1/p' \
+            >>$hop_output
     done
 }
 
@@ -31,9 +32,10 @@ ssh_transfer_file() {
     for i in {1..100}; do
         echo "ssh $1 $i"
         echo -n "$size," >> $ssh_output
-        { time scp -q -i ./containers/ssh_id_ed25519 \
-            $1 $2:$1; } \
-            2>>$ssh_output
+        ./pty_time/pty_time matches \
+            bash -c "ssh -i ./containers/ssh_id_ed25519 $2 \"cat $1 && echo 'done echoing file'\" | grep -m 1 \"done echoing file\"" \
+            | sed -nr 's/diff: ([[:digit:]]+) microseconds/\1/p' \
+            >>$ssh_output
     done
 }
 
@@ -43,8 +45,8 @@ ssh_transfer_file 1kB_file $1
 hop_transfer_file 1MB_file $1
 ssh_transfer_file 1MB_file $1
 
-hop_transfer_file 100MB_file $1
-ssh_transfer_file 100MB_file $1
+#hop_transfer_file 100MB_file $1
+#ssh_transfer_file 100MB_file $1
 
-hop_transfer_file 1GB_file $1
-ssh_transfer_file 1GB_file $1
+#hop_transfer_file 1GB_file $1
+#ssh_transfer_file 1GB_file $1
--- measurements/measure_time_to_shell.sh
+++ measurements/measure_time_to_shell.sh
@@ -5,11 +5,11 @@ echo -e "hop_time,ssh_time" > ./latency.csv
 for i in {1..100}
 do
     echo "hop iteration $i"
-    hop_time=$(./pty_time/pty_time ~\# ./hop -C ./containers/client_config $1 2>/dev/null \
+    hop_time=$(./pty_time/pty_time $ ./hop -C ./containers/client_config $1 2>/dev/null \
         | sed -nr 's/diff: ([[:digit:]]+) microseconds/\1/p')
 
     echo "ssh iteration $i"
-    ssh_time=$(./pty_time/pty_time ~\# ssh -i ./containers/ssh_id_ed25519 -p 7777 $1 \
+    ssh_time=$(./pty_time/pty_time $ ssh -i ./containers/ssh_id_ed25519 $1 \
         | sed -nr 's/diff: ([[:digit:]]+) microseconds/\1/p')
     
     echo -e "$hop_time,$ssh_time" >> ./latency.csv
