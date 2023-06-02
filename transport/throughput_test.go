2afcd9c93cd14eccd82ca2845c008de3fac11152
George Hosono <georgehosono@gmail.com>
2023-06-02 03:53:37 -0700

    lint fixes

--- transport/throughput_test.go
+++ transport/throughput_test.go
@@ -15,58 +15,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// TODO(hosono) Is there an easy way to generate these on the fly?
-var tlsCertificate []byte = []byte(`-----BEGIN CERTIFICATE-----
-MIIDDzCCAfegAwIBAgIUQzUbr+IxR50qEFApzoRFpzvvXAgwDQYJKoZIhvcNAQEL
-BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTIzMDYwMjA1NTE0M1oXDTIzMDcw
-MjA1NTE0M1owFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF
-AAOCAQ8AMIIBCgKCAQEAyMcCVRBEZ59k0hGEQj7rKcJpGYDPCvFmAP5WKiX8MSXq
-g9Z2p1PHSnZO9bYGJ00+woQBt7H36juBCw6xfuNklWaL9MfsrZ+xIGUpJjNkY8lx
-vQWgr2H60mkFa1dUGmVluMIFydRM0A/7z5blCYIMSXFg3LgRzttMnpHHVwN6kEGe
-6R+NknRbeqe8CFuAN6FpBz+4AAyOfdBQlH9lgg4UVEdMzlHMLFFHaDWm8DTY6Oh6
-u7ISGb/AVfEOY7J0Ph4H3U3/wNm1lWw3dDf167komevlVCyiU7vIYaWJjTaB4DAc
-9NN6IWjH3/bUzThAuGYW6QRLlav7HSAQAC0hCUtiTQIDAQABo1kwVzAUBgNVHREE
-DTALgglsb2NhbGhvc3QwCwYDVR0PBAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMB
-MB0GA1UdDgQWBBQvNW8VVFZ/HPlF2hSR3EWHr7DQ0jANBgkqhkiG9w0BAQsFAAOC
-AQEALaXQNYIQvJaYC1ZT8cnCwG25ThLsMphN2zN5hqdDlEtauUZPSuJuqUhSkCJz
-X82sFy/ekR0O3t/+jtXVAsUIgWpMC/q7GfHgJftlc1vi5jQEutO+KHJkqqnHNpjG
-nJLxUSMX4/0mgw4SrT7Qv/wo4F7VzHCIuHV+u+atIy1kvH8UwEM/i2flgXLRZ7s6
-pvJlZHk4t8+CQHQPd7XTL5w/y0vEuZhtaQr0BQg6LWoMlwfEx5t+628wGcjmVOZq
-vHlFJV4e2EgygKmeH2bkqQ521xyoxDoNT3rZjOODdh/DDh/jaBHSlZqUgkS7m8Mx
-L5MRY/7oKszJfn4nMNQmVi0uVA==
------END CERTIFICATE-----`)
-
-var tlsKey []byte = []byte(`-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDIxwJVEERnn2TS
-EYRCPuspwmkZgM8K8WYA/lYqJfwxJeqD1nanU8dKdk71tgYnTT7ChAG3sffqO4EL
-DrF+42SVZov0x+ytn7EgZSkmM2RjyXG9BaCvYfrSaQVrV1QaZWW4wgXJ1EzQD/vP
-luUJggxJcWDcuBHO20yekcdXA3qQQZ7pH42SdFt6p7wIW4A3oWkHP7gADI590FCU
-f2WCDhRUR0zOUcwsUUdoNabwNNjo6Hq7shIZv8BV8Q5jsnQ+HgfdTf/A2bWVbDd0
-N/XruSiZ6+VULKJTu8hhpYmNNoHgMBz003ohaMff9tTNOEC4ZhbpBEuVq/sdIBAA
-LSEJS2JNAgMBAAECggEACGUb0Zs1xQBq6oateUigtBULKHQUlCBOU83OpAIFFrf/
-Y4ZQ3UO5QkGKoSb8gGGwNVloKfpgw5Q7287AOK5A2ydXxEMkwAi8kv5v10dQVATN
-AuqMkrC92FToU1nqLBVfyLciH1OHZ663aHFmf7yGj36UXn46zmOogzvUsix00Dm/
-IsgQI5Cbgg6o9KCUoeXwH6vsNpoDvs/aH3BDBcAhaMfGxyVAUilH8FIkOzRN6Qv1
-fNuNRvn9qKB8A4wfaIMmGXzi9gnNrdUsJhTi5lljpWIjuyPvA3qhw5GJU77XD+wm
-3J+cmGxXlJTuJRP4r28CXsaCslrVmVkf+0tTj3vbqQKBgQD5/CBR4rUnVb/jU9CK
-Y5TT2B/v4DEpUQn0LtQSFF09pQcS4nfA8iKdpinwz+kj9+beetgXBcmSTfProsZB
-Re+wWXntn6p3e7nFitw14YigZSzgecePS6brveCWcLZSzqG8G8stoQ6j4SwP8ARF
-atLafwJFXxkCraQfhcDXVBb8KQKBgQDNm8Vvhhq0L+nsh9ZB8ivUlYioqYo5xe5r
-+LP+k6EVAWX2njEdzZfG62ahMM4FHI2fojeNxXxAVLjCzlzY0+2LDD/WiIgLGj+D
-nVsw/Q9kGKVpHZi+6cCefsr2NvJpDydmi07D8DUlJZWpI+mpwR7BxVzPUSBegWZ3
-2rxdQDN5hQKBgFkT74xeRYELvEHMJv55N5o5ZD/82mfHmc1qNmVu3j7OJHQlQNj/
-LziP8lf+LiyD9L+IdKHXjRlWL7nimdS+kAd+CsWS5JKJAwySS5/jiuTkyJYArwyv
-v343feT9qupc+SLnoIHU5zucTDAtrcfypcn4Ah4oIehjaZ1V8v6H11zpAoGBAMeL
-Sk42JGyDo89+9Z4C3i7vATZPdGslgYMgV+/WbxrnxIFYfeqiwY64n14I86laUMiA
-stHQvAnjL31AsjNtWrj8JmibEQ1VaBbf/MTKlMlccgxWQQvn6JzqCShzo0f7AQ4w
-XT0kPy/SDjGdPQUW/DaqyKwb3AJcK4peqzkFOe/RAoGADRKIOTDp+Kfo4giRQrJ1
-f6MPRJlRASm6PkOMtj1wiTkRHmHu273meGWZ9TtET9QPG6DApefuvwLCQgLk7rvV
-REEVks8E+GNtCyClf6StMyNLcqxkDGD98HM2zJ4XddTMuIIlbqC2dFIkODw0g/0l
-ir0xkE4wvTLMicn1gtlSEFQ=
------END PRIVATE KEY-----`)
-
-func measureThroughput(b *testing.B, w net.Conn , r net.Conn) {
-	var nBytes int64 = 10 << 30  // 10 GiB
+func measureThroughput(b *testing.B, w net.Conn, r net.Conn) {
+	var nBytes int64 = 10 << 30 // 10 GiB
 
 	b.ResetTimer()
 
@@ -89,8 +39,8 @@ func measureThroughput(b *testing.B, w net.Conn , r net.Conn) {
 	err = w.Close()
 	assert.NilError(b, err)
 
-	bytesPerSecs := float64(nBytes)/float64(b.Elapsed().Seconds())
-	b.ReportMetric(bytesPerSecs/(1 << 20), "MiB/secs")
+	bytesPerSecs := float64(nBytes) / b.Elapsed().Seconds()
+	b.ReportMetric(bytesPerSecs/(1<<20), "MiB/secs")
 }
 
 func BenchmarkTCP(b *testing.B) {
@@ -101,6 +51,7 @@ func BenchmarkTCP(b *testing.B) {
 	assert.NilError(b, err)
 
 	c2, err := listener.AcceptTCP()
+	assert.NilError(b, err)
 
 	measureThroughput(b, c1, c2)
 }
@@ -130,7 +81,7 @@ func BenchmarkTLS(b *testing.B) {
 		addr := "localhost:" + fmt.Sprint(l.Addr().(*net.TCPAddr).Port)
 		c1, err = tls.Dial("tcp", addr, clientConfig)
 		assert.NilError(b, err)
-		ch<-struct{}{}
+		ch <- struct{}{}
 	}()
 	c2, err := listener.Accept()
 	assert.NilError(b, err)
