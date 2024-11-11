859756f1bd59b221d441338eb75cfc4828a9dd6e
George Hosono <georgehosono@gmail.com>
2024-11-11 16:14:01 -0500

    moved to variables for script

--- containers/container_cred_gen.sh
+++ containers/container_cred_gen.sh
@@ -2,48 +2,52 @@
 set -e
 set -o pipefail
 
-HOME=${PWD}
+HOP_DIR=${PWD}
 
-echo ${HOME}
-mkdir -p ${HOME}/containers/CAFiles
+echo ${HOP_DIR}
+mkdir -p ${HOP_DIR}/containers/CAFiles
 
 # Generate CAFiles used to sign other certificates and such
 CA_CERT_DNS_NAME=${CA_CERT_DNS_NAME:='super_ca.com'}
-CAFILES_OUTPUT_DIR=${HOME}/${CAFILES_OUTPUT_DIR:='./containers/CAFiles'}
+CAFILES_OUTPUT_DIR=${HOP_DIR}/${CAFILES_OUTPUT_DIR:='./containers/CAFiles'}
+
+# Variables for Hop commands
+HOP_GEN="go run ${HOP_DIR}/cmd/hop-gen"
+HOP_ISSUE="go run ${HOP_DIR}/cmd/hop-issue"
 
 ## CA private keygen
-go run ${HOME}/cmd/hop-gen -signing | tee $CAFILES_OUTPUT_DIR/root-key.pem
-go run ${HOME}/cmd/hop-gen -signing | tee $CAFILES_OUTPUT_DIR/intermediate-key.pem
+${HOP_GEN} -signing | tee $CAFILES_OUTPUT_DIR/root-key.pem
+${HOP_GEN} -signing | tee $CAFILES_OUTPUT_DIR/intermediate-key.pem
 
 ## CA public keygen
-go run ${HOME}/cmd/hop-gen -signing -private $CAFILES_OUTPUT_DIR/root-key.pem | tee $CAFILES_OUTPUT_DIR/root.pub
-go run ${HOME}/cmd/hop-gen -signing -private $CAFILES_OUTPUT_DIR/intermediate-key.pem | tee $CAFILES_OUTPUT_DIR/intermediate.pub
+${HOP_GEN} -signing -private $CAFILES_OUTPUT_DIR/root-key.pem | tee $CAFILES_OUTPUT_DIR/root.pub
+${HOP_GEN} -signing -private $CAFILES_OUTPUT_DIR/intermediate-key.pem | tee $CAFILES_OUTPUT_DIR/intermediate.pub
 
 ## CA cert gen
-go run ${HOME}/cmd/hop-issue -type root -key-file $CAFILES_OUTPUT_DIR/root-key.pem -dns-name $CA_CERT_DNS_NAME | tee $CAFILES_OUTPUT_DIR/root.cert
-go run ${HOME}/cmd/hop-issue -type intermediate -key-file $CAFILES_OUTPUT_DIR/root-key.pem -cert-file $CAFILES_OUTPUT_DIR/root.cert -public-key $CAFILES_OUTPUT_DIR/intermediate.pub -dns-name $CA_CERT_DNS_NAME | tee $CAFILES_OUTPUT_DIR/intermediate.cert
+${HOP_ISSUE} -type root -key-file $CAFILES_OUTPUT_DIR/root-key.pem -dns-name $CA_CERT_DNS_NAME | tee $CAFILES_OUTPUT_DIR/root.cert
+${HOP_ISSUE} -type intermediate -key-file $CAFILES_OUTPUT_DIR/root-key.pem -cert-file $CAFILES_OUTPUT_DIR/root.cert -public-key $CAFILES_OUTPUT_DIR/intermediate.pub -dns-name $CA_CERT_DNS_NAME | tee $CAFILES_OUTPUT_DIR/intermediate.cert
 
 
 # Generate files for make serve-dev (simple hop connection)
 ## server
 EXAMPLE_CERT_DNS_NAME=${EXAMPLE_CERT_DNS_NAME:='example.com'}
-EXAMPLE_CERT_OUTPUT_DIR=${HOME}/${EXAMPLE_CERT_OUTPUT_DIR:='./containers'}
+EXAMPLE_CERT_OUTPUT_DIR=${HOP_DIR}/${EXAMPLE_CERT_OUTPUT_DIR:='./containers'}
 
 ## client
 EX_CLIENT_CERT_DNS_NAME=${EX_CLIENT_CERT_DNS_NAME:='root'}
-EX_CLIENT_CERT_OUTPUT_DIR=${HOME}/${EX_CLIENT_CERT_OUTPUT_DIR:='./containers'}
+EX_CLIENT_CERT_OUTPUT_DIR=${HOP_DIR}/${EX_CLIENT_CERT_OUTPUT_DIR:='./containers'}
 
 ## private keys
-go run ${HOME}/cmd/hop-gen | tee $EXAMPLE_CERT_OUTPUT_DIR/id_server.pem
-go run ${HOME}/cmd/hop-gen | tee $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pem
+${HOP_GEN} | tee $EXAMPLE_CERT_OUTPUT_DIR/id_server.pem
+${HOP_GEN} | tee $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pem
 
 ## public keys
-go run ${HOME}/cmd/hop-gen -private $EXAMPLE_CERT_OUTPUT_DIR/id_server.pem | tee $EXAMPLE_CERT_OUTPUT_DIR/id_server.pub
-go run ${HOME}/cmd/hop-gen -private $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pem | tee $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pub
+${HOP_GEN} -private $EXAMPLE_CERT_OUTPUT_DIR/id_server.pem | tee $EXAMPLE_CERT_OUTPUT_DIR/id_server.pub
+${HOP_GEN} -private $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pem | tee $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pub
 
 ## certs
-go run ${HOME}/cmd/hop-issue -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $EXAMPLE_CERT_OUTPUT_DIR/id_server.pub -dns-name $EXAMPLE_CERT_DNS_NAME | tee $EXAMPLE_CERT_OUTPUT_DIR/id_server.cert
-go run ${HOME}/cmd/hop-issue -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pub -dns-name $EX_CLIENT_CERT_DNS_NAME | tee $EX_CLIENT_CERT_OUTPUT_DIR/id_client.cert
+${HOP_ISSUE} -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $EXAMPLE_CERT_OUTPUT_DIR/id_server.pub -dns-name $EXAMPLE_CERT_DNS_NAME | tee $EXAMPLE_CERT_OUTPUT_DIR/id_server.cert
+${HOP_ISSUE} -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $EX_CLIENT_CERT_OUTPUT_DIR/id_client.pub -dns-name $EX_CLIENT_CERT_DNS_NAME | tee $EX_CLIENT_CERT_OUTPUT_DIR/id_client.cert
 
 
 # Authgrant tests (authgrant-dev and authgrant-chain-dev) setup
@@ -52,31 +56,31 @@ TARGET_CERT_DNS_NAME=${TARGET_CERT_DNS_NAME:='target.com'}
 DELEGATE_CERT_DNS_NAME=${DELEGATE_CERT_DNS_NAME:='delegate.com'}
 THIRD_CERT_DNS_NAME=${THIRD_CERT_DNS_NAME:='third.com'}
 
-TARGET_CERT_OUTPUT_DIR=${HOME}/${TARGET_CERT_OUTPUT_DIR:='./containers/target_server'}
-DELEGATE_CERT_OUTPUT_DIR=${HOME}/${DELEGATE_CERT_OUTPUT_DIR:='./containers/delegate_proxy_server'}
-THIRD_CERT_OUTPUT_DIR=${HOME}/${THIRD_CERT_OUTPUT_DIR:='./containers/third_server'}
+TARGET_CERT_OUTPUT_DIR=${HOP_DIR}/${TARGET_CERT_OUTPUT_DIR:='./containers/target_server'}
+DELEGATE_CERT_OUTPUT_DIR=${HOP_DIR}/${DELEGATE_CERT_OUTPUT_DIR:='./containers/delegate_proxy_server'}
+THIRD_CERT_OUTPUT_DIR=${HOP_DIR}/${THIRD_CERT_OUTPUT_DIR:='./containers/third_server'}
 
 ## client
 PRINCIPAL_CERT_DNS_NAME=${PRINCIPAL_CERT_DNS_NAME:='principal'}
-PRINCIPAL_CERT_OUTPUT_DIR=${HOME}/${PRINCIPAL_CERT_OUTPUT_DIR:='./containers/principal_client'}
+PRINCIPAL_CERT_OUTPUT_DIR=${HOP_DIR}/${PRINCIPAL_CERT_OUTPUT_DIR:='./containers/principal_client'}
 
 ## Private Keys
-go run ${HOME}/cmd/hop-gen | tee $TARGET_CERT_OUTPUT_DIR/id_server.pem
-go run ${HOME}/cmd/hop-gen | tee $DELEGATE_CERT_OUTPUT_DIR/id_server.pem
-go run ${HOME}/cmd/hop-gen | tee $THIRD_CERT_OUTPUT_DIR/id_server.pem
+${HOP_GEN} | tee $TARGET_CERT_OUTPUT_DIR/id_server.pem
+${HOP_GEN} | tee $DELEGATE_CERT_OUTPUT_DIR/id_server.pem
+${HOP_GEN} | tee $THIRD_CERT_OUTPUT_DIR/id_server.pem
 
-go run ${HOME}/cmd/hop-gen | tee $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pem
+${HOP_GEN} | tee $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pem
 
 ## Public Keys
-go run ${HOME}/cmd/hop-gen -private $TARGET_CERT_OUTPUT_DIR/id_server.pem | tee $TARGET_CERT_OUTPUT_DIR/id_server.pub
-go run ${HOME}/cmd/hop-gen -private $DELEGATE_CERT_OUTPUT_DIR/id_server.pem | tee $DELEGATE_CERT_OUTPUT_DIR/id_server.pub
-go run ${HOME}/cmd/hop-gen -private $THIRD_CERT_OUTPUT_DIR/id_server.pem | tee $THIRD_CERT_OUTPUT_DIR/id_server.pub
+${HOP_GEN} -private $TARGET_CERT_OUTPUT_DIR/id_server.pem | tee $TARGET_CERT_OUTPUT_DIR/id_server.pub
+${HOP_GEN} -private $DELEGATE_CERT_OUTPUT_DIR/id_server.pem | tee $DELEGATE_CERT_OUTPUT_DIR/id_server.pub
+${HOP_GEN} -private $THIRD_CERT_OUTPUT_DIR/id_server.pem | tee $THIRD_CERT_OUTPUT_DIR/id_server.pub
 
-go run ${HOME}/cmd/hop-gen -private $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pem | tee $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pub
+${HOP_GEN} -private $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pem | tee $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pub
 
 ## Certs
-go run ${HOME}/cmd/hop-issue -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $TARGET_CERT_OUTPUT_DIR/id_server.pub -dns-name $TARGET_CERT_DNS_NAME | tee $TARGET_CERT_OUTPUT_DIR/id_server.cert
-go run ${HOME}/cmd/hop-issue -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $DELEGATE_CERT_OUTPUT_DIR/id_server.pub -dns-name $DELEGATE_CERT_DNS_NAME | tee $DELEGATE_CERT_OUTPUT_DIR/id_server.cert
-go run ${HOME}/cmd/hop-issue -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $THIRD_CERT_OUTPUT_DIR/id_server.pub -dns-name $THIRD_CERT_DNS_NAME | tee $THIRD_CERT_OUTPUT_DIR/id_server.cert
+${HOP_ISSUE} -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $TARGET_CERT_OUTPUT_DIR/id_server.pub -dns-name $TARGET_CERT_DNS_NAME | tee $TARGET_CERT_OUTPUT_DIR/id_server.cert
+${HOP_ISSUE} -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $DELEGATE_CERT_OUTPUT_DIR/id_server.pub -dns-name $DELEGATE_CERT_DNS_NAME | tee $DELEGATE_CERT_OUTPUT_DIR/id_server.cert
+${HOP_ISSUE} -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $THIRD_CERT_OUTPUT_DIR/id_server.pub -dns-name $THIRD_CERT_DNS_NAME | tee $THIRD_CERT_OUTPUT_DIR/id_server.cert
 
-go run ${HOME}/cmd/hop-issue -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pub -dns-name $PRINCIPAL_CERT_DNS_NAME | tee $PRINCIPAL_CERT_OUTPUT_DIR/id_client.cert
+${HOP_ISSUE} -type leaf -key-file $CAFILES_OUTPUT_DIR/intermediate-key.pem -cert-file $CAFILES_OUTPUT_DIR/intermediate.cert -public-key $PRINCIPAL_CERT_OUTPUT_DIR/id_client.pub -dns-name $PRINCIPAL_CERT_DNS_NAME | tee $PRINCIPAL_CERT_OUTPUT_DIR/id_client.cert
