e502562b40b5f4be00438a2cc5e3d377a12c2169
Paul Flammarion <pflammarion@cs.stanford.edu>
2024-11-19 23:39:15 +0100

    update comment

--- hopserver/hopserver.go
+++ hopserver/hopserver.go
@@ -122,7 +122,7 @@ func NewHopServer(sc *config.ServerConfig) (*HopServer, error) {
 		var certificates []*transport.Certificate
 
 		// vhosts.Match is based on patterns and can be "*".
-		// If the configuration has more HiddenModeVHostNames than vhosts return
+		// If the configuration has more HiddenModeVHostNames than vhosts: return
 		if len(sc.HiddenModeVHostNames) > len(vhosts) {
 			return nil, fmt.Errorf("number of server Hidden Mode VHost Names exceed the number of current vhosts")
 		}
