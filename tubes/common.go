709e0c03bf73af46b19347e5f1a712c83d1ea539
Paul Flammarion <pflammarion@cs.stanford.edu>
2025-01-27 19:19:49 +0100

    update min RTT to 10ms

--- tubes/common.go
+++ tubes/common.go
@@ -36,7 +36,7 @@ const initialRTT = 333 * time.Millisecond
 // The minimum value that a Reliable tube will use for its RTT.
 // If the measured RTT is ever lower than minRTT, it is clamped to minRTT
 // From RFC 9002 section 6.2.1 (kGranularity)
-const minRTT = time.Millisecond
+const minRTT = 10 * time.Millisecond
 
 // the maximum number of packets to retransmit per rto
 // even if the window is larger, no more packets will be transmitted
