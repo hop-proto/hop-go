047265848f35478058bb4137696251e25cf0947c
Paul Flammarion <pflammarion@cs.stanford.edu>
2025-01-27 22:11:36 +0100

    re include the retransmission in the stack for reliable tubes

--- tubes/reliable.go
+++ tubes/reliable.go
@@ -147,30 +147,27 @@ func (r *Reliable) send() {
 	for ok {
 		select {
 		// find a way of having a logic retransmit ticker time and logic
-		/*
-			case <-r.sender.RetransmitTicker.C:
-				r.l.Lock()
+		case <-r.sender.RetransmitTicker.C:
+			r.l.Lock()
 
-				numFrames := r.sender.framesToSend(true, 0)
+			numFrames := r.sender.framesToSend(true, 0)
 
-				r.log.WithField("numFrames", numFrames).Trace("retransmitting")
-				for i := 0; i < numFrames; i++ {
-					frameEntry := r.sender.frames[i]
-					// Only retransmit the timed out frames, however will be sent by the windowOpen
-					if frameEntry.queued {
-						//logrus.Debugf("retransmitting frame %v with ack %v", frameEntry.frame.frameNo, r.recvWindow.getAck())
-						r.sendOneFrame(frameEntry.frame, true)
-						frameEntry.Time = time.Now()
-					}
+			r.log.WithField("numFrames", numFrames).Trace("retransmitting")
+			for i := 0; i < numFrames; i++ {
+				frameEntry := r.sender.frames[i]
+				// Only retransmit the timed out frames, however will be sent by the windowOpen
+				if frameEntry.queued {
+					//logrus.Debugf("retransmitting frame %v with ack %v", frameEntry.frame.frameNo, r.recvWindow.getAck())
+					r.sendOneFrame(frameEntry.frame, true)
+					frameEntry.Time = time.Now()
 				}
+			}
 
-				// Back off RTT if no ACKs were received
-				r.sender.RTT *= 2
-				r.sender.resetRetransmitTicker()
+			// Back off RTT if no ACKs were received
+			r.sender.RTT *= 2
+			r.sender.resetRetransmitTicker()
 
-				r.l.Unlock()
-
-		*/
+			r.l.Unlock()
 
 		case <-r.sender.windowOpen:
 			r.l.Lock()
