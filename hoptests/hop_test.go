c3ef4cfabf7a3f36673bd2ee8a4217c130c9a29d
George Hosono <georgehosono@gmail.com>
2025-07-16 21:07:13 -0400

    test failing in CI

--- hoptests/hop_test.go
+++ hoptests/hop_test.go
@@ -553,7 +553,10 @@ func TestSelfAuthGrant(t *testing.T) {
 	err = s.Server.Close()
 	assert.NilError(t, err)
 
-	outString := output.String()
+	buf := make([]byte, len(testString))
+	_, err = output.Read(buf)
+	assert.NilError(t, err)
+	outString := string(buf)
 	logrus.Info(outString)
 	assert.Equal(t, outString, testString)
 }
